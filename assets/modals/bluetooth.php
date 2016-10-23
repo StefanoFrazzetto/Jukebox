933765bd9a0ef35502a84391c276e85411378fc3
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-10-23 17:45:14 +0100

    Implemented continuous scan and improved the UI

--- assets/modals/bluetooth.php
+++ assets/modals/bluetooth.php
@@ -1,48 +1,92 @@
+<style>
+    .bt-no {
+        width: 10%;
+    }
+
+    .bt-name {
+        width: 60%;
+    }
+
+    .bt-mac {
+        width: 30%;
+    }
+</style>
 <div class="modalHeader">Bluetooth</div>
-<div class="modalBody mCustomScrollbar" data-mcs-theme="dark">
-    <div class="top" style="margin-bottom: 80px">
-        <div class="onoffswitch pull-left">
-            <input type="checkbox" name="bluetooth" class="onoffswitch-checkbox" id="bluetooth">
-            <label class="onoffswitch-label" for="bluetooth">
-                <span class="onoffswitch-inner"></span>
-                <span class="onoffswitch-switch"></span>
-            </label>
-        </div>
-        <div id="btn_scan" class="box-btn pull-right">SCAN DEVICES</div>
-        <div id="btn_unpair" class="box-btn pull-right">UNPAIR ALL</div>
-    </div>
-    <table class="songsTable text-center" id="devices" style="width: 100%; padding-top: 0; margin-top: 0; margin-bottom: 30px">
-        <thead>
+<div class="modalBody" data-mcs-theme="dark">
+    <div class="mCustomScrollbar overflow-hidden">
+        <table class="songsTable text-center" id="devices"
+               style="min-width: 100%; padding-top: 0; margin-top: 0; margin-bottom: 30px">
+
+            <thead>
             <tr class="th">
-                <th>#</th>
-                <th>Device name</th>
-                <th>MAC</th>
+                <th class="bt-no">#</th>
+                <th class="bt-name">Device name</th>
+                <th class="bt-mac">MAC</th>
             </tr>
-        </thead>
-        <tbody>
-            <tr> <td>#</td> <td></td> <td></td> </tr>
-        </tbody>
-    </table>
+
+            </thead>
+
+            <tr>
+                <td></td>
+                <td>Click scan devices to start</td>
+                <td></td>
+            </tr>
+
+        </table>
+    </div>
+</div>
+<div class="modalFooter">
+    <div class="onoffswitch pull-left inline">
+        <input type="checkbox" name="bluetooth" class="onoffswitch-checkbox" id="bluetooth">
+        <label class="onoffswitch-label" for="bluetooth">
+            <span class="onoffswitch-inner"></span>
+            <span class="onoffswitch-switch"></span>
+        </label>
+    </div>
+    <div id="btn_scan" class="box-btn pull-right">SCAN DEVICES</div>
+    <div id="btn_unpair" class="box-btn pull-right">UNPAIR ALL</div>
 </div>
 <script type="text/javascript">
+    var bluetooth_switch_on;
+    var continue_scanning;
+
     $(document).ready(function () {
-        toggleBluetooth();
+        initBluetooth();
     });
 
-    $('#btn_unpair').click(function () {
-        bluetooth({action: 'unpair'});
-    });
+    /**
+     * Initialize the bluetooth modal.
+     */
+    function initBluetooth() {
+        // Turn ON the bluetooth interface when this modal is opened.
+        bluetooth_switch_on = true;
+        toggleBluetooth(bluetooth_switch_on);
 
-    $('#btn_scan').click(function () {
-        console.log("FIRE!");
+        startScan();
+    }
+
+    /**
+     * Start the scan in continuous mode.
+     */
+    function startScan() {
         if ($('#bluetooth').prop('checked')) {
-            $('#devices').find('tbody').html('<tr><td colspan="3">Loading...</td></tr>');
+            $('#btn_scan').addClass("active");
+            $('#btn_scan').text("SCANNING...");
 
-            bluetooth({action: 'scan'});
+            $('#devices').find('tbody').html('<tr><td></td><td>SCANNING...</td><td></td></tr>');
+
+            continue_scanning = true;
+            bluetooth({action: "scan", continous: true});
         } else {
-            alert('Turn on the bluetooth');
+            alert('You need to turn ON the bluetooth.');
         }
-    });
+    }
+
+    function stopScan() {
+        continue_scanning = false;
+        $('#btn_scan').removeClass("active");
+        $('#btn_scan').text("SCAN FOR DEVICES");
+    }
 
     function bluetooth(data) {
         $.ajax({
@@ -54,11 +98,11 @@
             var output = JSON.parse(res);
             var devices = $('#devices');
 
-            switch (data.action){
+            switch (data.action) {
 
                 case 'scan':
-                    if(output == null) {
-                        devices.find('tbody').html('<tr><td colspan="3">NO DEVICES FOUND</td></td>');
+                    if (output == null) {
+                        devices.find('tbody').html('<tr><td></td><td>NO DEVICES FOUND.</td><td></td></td>');
                     } else {
                         devices.find('tbody').html('');
 
@@ -67,11 +111,17 @@
                         });
 
                         devices.find('tbody tr').click(function () {
+                            stopScan();
                             var mac_address = $(this).find('.mac').html();
                             console.log(mac_address);
                             bluetooth({action: 'pair', mac: mac_address});
                         });
                     }
+
+                    if (data.continous && continue_scanning) {
+                        setTimeout(bluetooth({action: "scan", continous: true}), 500);
+                    }
+
                     break;
 
                 case 'pair':
@@ -95,11 +145,24 @@
         });
     }
 
+    $('#btn_unpair').click(function () {
+        bluetooth({action: 'unpair'});
+    });
+
+    $('#btn_scan').click(function () {
+        if (continue_scanning) {
+            stopScan();
+        } else {
+            startScan();
+        }
+    });
+
     function toggleBluetooth() {
-        var bluetooth_on = $('#bluetooth').prop('checked');
-        if (bluetooth_on) {
+        if (bluetooth_switch_on) {
+            $('#bluetooth').prop("checked", "true");
             var mode = 'turn_on';
         } else {
+            $('#bluetooth').removeProp("checked");
             mode = 'turn_off';
         }
 
@@ -107,6 +170,7 @@
     }
 
     $('#bluetooth').change(function () {
+        bluetooth_switch_on = !bluetooth_switch_on;
         toggleBluetooth();
     });
 </script>
