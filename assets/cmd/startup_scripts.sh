441fdf3c9060d2cf194209654410a5b3d1c3379d
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-10-24 11:44:36 +0100

    Set volume to 88% at startup

--- assets/cmd/startup_scripts.sh
+++ assets/cmd/startup_scripts.sh
@@ -6,13 +6,18 @@ THIS_SCRIPT_PATH=/var/www/html/assets/cmd/startup_script.sh
 PHP=$(which php)
 PHP_STARTUP_FILE=/var/www/html/assets/php/startup_scripts.php
 
+
 # If this script is not added to /etc/rc.local, then add it and make it executable.
 if ! grep -q "$THIS_SCRIPT_PATH" "$RC_LOCAL" ; then
     echo -e "# Startup script\n$THIS_SCRIPT_PATH\n" | cat - "$RC_LOCAL" > /tmp/out && mv /tmp/out "$RC_LOCAL"
     chmod +x "$THIS_SCRIPT_PATH"
 fi
 
+# Turn on the speakers
 ./int_speakers_on.sh
+
+# Set volume to 88%
+amixer set Master 88%
 
 # Execute PHP scripts.
 $PHP "$PHP_STARTUP_FILE"
