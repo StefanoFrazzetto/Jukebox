f8dccf7a85b2a530056f58c0fc8fc163c3f524dc
bass-bunny <info.freelancewd@gmail.com>
2016-10-26 03:47:00 +0800

    Force push to fix permissions?

--- assets/cmd/startup_scripts.sh
+++ assets/cmd/startup_scripts.sh
@@ -1,23 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-RC_LOCAL=/etc/rc.local
-THIS_SCRIPT_PATH=/var/www/html/assets/cmd/startup_script.sh
+RC_LOCAL="/etc/rc.local"
+THIS_SCRIPT_PATH="/var/www/html/assets/cmd/startup_script.sh"
 
 PHP=$(which php)
-PHP_STARTUP_FILE=/var/www/html/assets/php/startup_scripts.php
+PHP_STARTUP_FILE="/var/www/html/assets/php/startup_scripts.php"
 
 
 # If this script is not added to /etc/rc.local, then add it and make it executable.
 if ! grep -q "$THIS_SCRIPT_PATH" "$RC_LOCAL" ; then
-    echo -e "# Startup script\n$THIS_SCRIPT_PATH\n" | cat - "$RC_LOCAL" > /tmp/out && mv /tmp/out "$RC_LOCAL"
-    chmod +x "$THIS_SCRIPT_PATH"
+	echo -e "# Startup script\n$THIS_SCRIPT_PATH\n" | cat - $RC_LOCAL > /tmp/out && mv /tmp/out $RC_LOCAL
+	chmod +x $THIS_SCRIPT_PATH
 fi
 
 # Turn on the speakers
-./int_speakers_on.sh
+source ./int_speakers_on.sh
 
 # Set volume to 88%
-su -c amixer set Master 88% bananapi
+SPEAKERS_CMD="amixer set Master 88%"
+su - bananapi -c $SPEAKERS_CMD
 
 # Execute PHP scripts.
 $PHP "$PHP_STARTUP_FILE"
