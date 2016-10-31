100e8bf3e950e282c0531ff43f19dca8923365f8
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-10-31 14:44:45 +0000

    Created Utility class in php-lib

---
+++ assets/php-lib/Utility.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by Stefano
+ * Date: 31/10/2016
+ */
+abstract class Utility
+{
+    public static function cleanString($string)
+    {
+        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+        return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+    }
+}
