3657b17eb1e3acbf0ae56d84a4496168b8f9dffa
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-10-31 14:59:05 +0000

    Added require to Utility.php

--- assets/php/burner/autoload.php
+++ assets/php/burner/autoload.php
@@ -10,5 +10,6 @@ require_once "DiscWriter.php";
 require_once "MiscFunctions.php";
 require_once "TracksHandler.php";
 require_once "BurnerHandler.php";
+require_once "../../php-lib/Utility.php";
 
 $scripts = "./scripts/";
