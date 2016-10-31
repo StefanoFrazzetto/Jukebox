59bb0f61a97a65a5091c383978cddb3581a6e15c
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-10-31 15:52:53 +0000

    Removed html entities with regex

--- assets/php-lib/Utility.php
+++ assets/php-lib/Utility.php
@@ -8,6 +8,10 @@ abstract class Utility
 {
     public static function cleanString($string)
     {
+        $string = preg_replace_callback("/(&#[0-9]+;)/", function ($m) {
+            return mb_convert_encoding($m[1], "UTF-8", "HTML-ENTITIES");
+        }, $string);
+        $string = html_entity_decode($string, ENT_COMPAT, "UTF-8");
         $string = str_replace(' ', '_', $string); // Replaces all spaces with hyphens.
         return preg_replace('/[^A-Za-z0-9\_\-]+/', '', $string); // Replace special chars with hyphens.
     }
