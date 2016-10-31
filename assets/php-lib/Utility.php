5c21991b56b8e4279f14c87eb1157b37e96176f0
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-10-31 15:37:53 +0000

    Changed regex for cleanString

--- assets/php-lib/Utility.php
+++ assets/php-lib/Utility.php
@@ -9,6 +9,6 @@ abstract class Utility
     public static function cleanString($string)
     {
         $string = str_replace(' ', '_', $string); // Replaces all spaces with hyphens.
-        return preg_replace('/[^A-Za-z0-9\_\-]+/', '-', $string); // Replace special chars with hyphens.
+        return preg_replace('/[^A-Za-z0-9\_\-]+/', '', $string); // Replace special chars with hyphens.
     }
 }
