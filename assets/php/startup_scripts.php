c9fd6f60525ec0f8407f2a57385ac2048eaf5c53
Vittorio <pryderi.mail@gmail.com>
2016-11-12 17:26:35 +0000

    Updated the php start-up scripts to attempt a connection to the previously saved network

--- assets/php/startup_scripts.php
+++ assets/php/startup_scripts.php
@@ -2,3 +2,16 @@
 
 // Put all the stuff you want to execute at startup time here.
 // Example: create config folders, files, etc.
+
+include __DIR__ . "/../php-lib/Network.php";
+
+$network = new Network();
+
+$network->load_network();
+
+try {
+    $network->connect();
+} catch (Exception $e) {
+    error_log("Failed to connect at start up because of this: " . $e->getMessage());
+    error_log("Don't blame if it doesn't work, I am a good program, and I catch my exceptions.");
+}
