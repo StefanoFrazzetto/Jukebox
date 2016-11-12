f4e4f9e9f8d8b6f6f749e0a7ca2cc266c157f377
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-11-12 17:08:39 +0000

    Implement bluetooth helper

--- assets/cmd/bluetooth/connect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-MAC=$0
--- assets/php/Bluetooth.php
+++ assets/php/Bluetooth.php
@@ -5,21 +5,25 @@ class Bluetooth
     private $config;
 
     /** The bluetooth devices array */
-    protected $devices;
+    private $devices;
 
     /** Bluetooth scripts */
-    protected $cmd_folder;
+    private $cmd_folder;
+
+    /** Bluetooth helper */
+    private $helper;
 
     /**
-     * Bluetooth constructor.
+     * Handle the bluetooth actions.
      *
-     * @param $act
-     * @param $mac
+     * @param $act - the action to perform
+     * @param $mac - the device mac address
      */
     public function __construct($act, $mac = "")
     {
         $this->config = include "../config.php";
         $this->cmd_folder = $this->config['assets_path'] . '/cmd/bluetooth/';
+        $this->helper = $this->cmd_folder . 'bluetooth-helper.sh';
 
         switch ($act) {
             case 'turn_on':
@@ -50,60 +54,16 @@ class Bluetooth
         }
     }
 
-    private function pair($mac) {
-        $output = shell_exec($this->cmd_folder . "bluez5-connect $mac");
-        if (strpos($output, 'org.bluez.Error.Failed') !== false) {
-            $this->output('failed');
-        } else {
-            $this->output("connected");
-        }
-    }
-
-    private function unpair() {
-        $cmd = shell_exec($this->cmd_folder . 'unpair-all.sh');
-        if (strpos($cmd, 'done') === false) {
-            $this->output("Error unpairing the devices");
-        } else {
-            $this->output("All devices unpaired.");
-        }
-    }
-
-    /**
-     * Launch the scan.
-     *
-     * @return bool true if the scan is successful, false if the interface is off.
-     */
-    private function scan() {
-        $cmd = shell_exec('hcitool scan');
-
-        if(strpos($cmd, 'not available') === false) {
-            str_replace('Scanning ...', '', $cmd);
-            preg_match("/\\n\\t(.*)\\t(.*)\\n/", $cmd, $matches);
-            unset($matches[0]);
-
-            for ($i = 1; $i < count($matches); $i+=2) {
-                $device['mac'] = $matches[$i];
-                $device['device'] = $matches[$i+1];
-
-                $this->devices[] = $device;
-            }
-
-            $this->output($this->devices);
-            return true;
-        }
-
-        return false;
-    }
-
     /**
      * Change the bluetooth status.
      *
      * @param string $mode on or off
      * @return string
      */
-    private function power($mode) {
+    private function power($mode)
+    {
         $cmd = shell_exec($this->cmd_folder . "power.sh $mode");
-        if(strpos($cmd, 'succeeded') !== false) {
+        if (strpos($cmd, 'succeeded') !== false) {
             $this->output("Bluetooth is now $mode");
         } else {
             $this->output("Can't switch the bluetooth $mode");
@@ -115,8 +75,57 @@ class Bluetooth
      *
      * @param string $string
      */
-    private function output($string) {
+    private function output($string)
+    {
         echo json_encode($string);
+    }
+
+    /**
+     * Launch the scan.
+     *
+     * @return bool true if the scan is successful, false if the interface is off.
+     */
+    private function scan()
+    {
+        $cmd = shell_exec($this->helper . ' scan');
+
+        if (strpos($cmd, 'not available') === false) {
+            str_replace('Scanning ...', '', $cmd);
+            preg_match("/\\n\\t(.*)\\t(.*)\\n/", $cmd, $matches);
+            unset($matches[0]);
+
+            for ($i = 1; $i < count($matches); $i += 2) {
+                $device['mac'] = $matches[$i];
+                $device['device'] = $matches[$i + 1];
+
+                $this->devices[] = $device;
+            }
+
+            $this->output($this->devices);
+            return true;
+        }
+
+        return false;
+    }
+
+    private function pair($mac)
+    {
+        $output = shell_exec($this->cmd_folder . "bluez5-connect $mac");
+        if (strpos($output, 'org.bluez.Error.Failed') !== false) {
+            $this->output('failed');
+        } else {
+            $this->output("connected");
+        }
+    }
+
+    private function unpair()
+    {
+        $cmd = shell_exec($this->cmd_folder . 'unpair-all.sh');
+        if (strpos($cmd, 'done') === false) {
+            $this->output("Error unpairing the devices");
+        } else {
+            $this->output("All devices unpaired.");
+        }
     }
 
 }
