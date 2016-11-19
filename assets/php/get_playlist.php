48f83dd69f21ef03b18697e6b44334d404a33296
Vittorio <pryderi.mail@gmail.com>
2016-11-19 18:48:56 +0000

    Fixed possible bug when there is no album in the database

--- assets/php/get_playlist.php
+++ assets/php/get_playlist.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: application/json');
+
 require 'dbconnect.php';
 $albumID = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
@@ -8,29 +10,26 @@ $results = $mysqli->query("SELECT tracks FROM $albums WHERE id = $albumID LIMIT 
 $mysqli->query("UPDATE $albums SET hits = hits + 1 WHERE id = $albumID");
 
 
-// if you are a code nazi, please look away. There are some jews over there!
-$last_used_id = $mysqli->query("SELECT last_used_id FROM time_machine LIMIT 1")->fetch_object()->last_used_id;
+try {
+    // if you are a code nazi, please look away. There are some jews over there!
+    $last_used_id = $mysqli->query("SELECT last_used_id FROM time_machine LIMIT 1")->fetch_object()->last_used_id;
 
-$last_used_id ++;
+    $last_used_id++;
 
-$mysqli->query("UPDATE $albums SET last_played = $last_used_id WHERE id = $albumID");
+    $mysqli->query("UPDATE $albums SET last_played = $last_used_id WHERE id = $albumID");
 
-$mysqli->query("UPDATE time_machine SET last_used_id = $last_used_id");
-// Okay, you can look again!
-// No, no I HAD GOOD REASONS FOR DOING THAT, keep that gun away, sir, please, I have a family!
+    $mysqli->query("UPDATE time_machine SET last_used_id = $last_used_id");
+} finally {
+    // Okay, you can look again!
+    // No, no I HAD GOOD REASONS FOR DOING THAT, keep that gun away, sir, please, I have a family!
+    $tracks = $results->fetch_object();
 
+    $tracks = json_decode($tracks->tracks);
 
-$tracks = $results->fetch_object();
+    foreach ($tracks as $key => $track) {
+        $track->album = $albumID;
+        $track->no = $key;
+    }
 
-$tracks = json_decode($tracks->tracks);
-
-foreach($tracks as $key => $track){
-    $track->album = $albumID;
-    $track->no = $key;
+    echo json_encode($tracks);
 }
-
-$tracks = json_encode($tracks);
-
-
-header('Content-Type: application/json');
-echo $tracks;
