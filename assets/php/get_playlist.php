c60c935461c17131efe331b8b8d5a737c450e8d5
Vittorio <pryderi.mail@gmail.com>
2016-11-19 21:18:40 +0000

    Moved the function to check the last played album from database to file

--- assets/php/get_playlist.php
+++ assets/php/get_playlist.php
@@ -7,21 +7,33 @@ $albumID = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 $results = $mysqli->query("SELECT tracks FROM $albums WHERE id = $albumID LIMIT 1");
 
+// TODO the hits shouldn't be increased if the
 $mysqli->query("UPDATE $albums SET hits = hits + 1 WHERE id = $albumID");
-
 
 try {
     // if you are a code nazi, please look away. There are some jews over there!
-    $last_used_id = $mysqli->query("SELECT last_used_id FROM time_machine LIMIT 1")->fetch_object()->last_used_id;
+    $last_used_id_file = __DIR__ . '/../config/lastid';
+    $last_used_id = 0;
 
-    $last_used_id++;
+    if (file_exists($last_used_id_file)) {
+        $last_used_id = intval(file_get_contents($last_used_id_file));
+        $last_used_id++;
+    }
+
+    file_put_contents($last_used_id_file, $last_used_id);
 
     $mysqli->query("UPDATE $albums SET last_played = $last_used_id WHERE id = $albumID");
-
-    $mysqli->query("UPDATE time_machine SET last_used_id = $last_used_id");
-} finally {
     // Okay, you can look again!
     // No, no I HAD GOOD REASONS FOR DOING THAT, keep that gun away, sir, please, I have a family!
+} catch (Exception $e) {
+    error_log($e->getTraceAsString());
+    exit;
+} finally {
+    if ($results === false) {
+        echo '{"error": "true"}';
+        exit;
+    }
+
     $tracks = $results->fetch_object();
 
     $tracks = json_decode($tracks->tracks);
