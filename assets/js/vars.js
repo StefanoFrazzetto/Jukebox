15a89bbd32551bd870273997a08085195558d9e9
Vittorio <pryderi.mail@gmail.com>
2016-11-21 18:14:59 +0000

    Added new support var to check if is a jukebox browser via js

--- assets/js/vars.js
+++ assets/js/vars.js
@@ -1,3 +1,4 @@
+var isJukebox = false;
 var animation_short = 200;
 var animation_medium = 400;
 var animation_long = 800;
--- assets/js/vars_jb.js
+++ assets/js/vars_jb.js
@@ -1,3 +1,4 @@
+isJukebox = true;
 animation_short = 0;
 animation_medium = 0;
 animation_long = 0;
