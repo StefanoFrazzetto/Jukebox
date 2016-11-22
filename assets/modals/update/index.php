44375e9ecdf43071eb2d2d50ad5c5b030db229c5
Vittorio <pryderi.mail@gmail.com>
2016-11-22 22:11:04 +0000

    Added change list to the update modal

--- assets/modals/update/index.php
+++ assets/modals/update/index.php
@@ -11,30 +11,48 @@
 <div class="modalHeader">Update</div>
 <div class="modalBody">
     <div class="center">
-        <div id="loader">
-            <i class="fa fa-spinner fa-5x fa-spin"></i>
-            <p>Checking for updates</p>
-        </div>
+        <div class="col-left">
+            <div id="loader">
+                <i class="fa fa-spinner fa-5x fa-spin"></i>
+                <p>Checking for updates</p>
+            </div>
 
-        <div id="up-to-date" class="hidden">
-            <h2><i class="fa fa-check"></i> Up to date</h2>
-            <p>Congrats, your jukebox is running the latest version.</p>
-            <button class="check-update-btn">
-                <i class="fa fa-refresh"></i> Check for update
-            </button>
-        </div>
+            <div id="up-to-date" class="hidden">
+                <h2><i class="fa fa-check"></i> Up to date</h2>
+                <p>Congrats, your jukebox is running the latest version.</p>
+                <button class="check-update-btn">
+                    <i class="fa fa-refresh"></i> Check for update
+                </button>
+            </div>
 
-        <div id="not-up-to-date" class="hidden">
-            <h2><i class="fa fa-close"></i> NOT up to date</h2>
-            <p>The jukebox needs an update.</p>
-            <button id="update-btn"><i class="fa fa-arrow-circle-o-up"></i> UPDATE</button>
-        </div>
+            <div id="not-up-to-date" class="hidden">
+                <h2><i class="fa fa-close"></i> NOT up to date</h2>
+                <p>The jukebox needs an update.</p>
+                <button class="update-btn"><i class="fa fa-arrow-circle-o-up"></i> UPDATE</button>
+            </div>
 
-        <div id="error" class="hidden">
-            <h2><i class="fa fa-warning"></i> Error</h2>
-            <p id="errorMessage">The jukebox needs an update.</p>
-            <button class="update-btn"><i class="fa fa-arrow-circle-o-up"></i> UPDATE</button>
-            <button class="check-update-btn"><i class="fa fa-refresh"></i> Check for update</button>
+            <div id="error" class="hidden">
+                <h2><i class="fa fa-warning"></i> Error</h2>
+                <p id="errorMessage">The jukebox needs an update.</p>
+                <button class="update-btn"><i class="fa fa-arrow-circle-o-up"></i> UPDATE</button>
+                <button class="check-update-btn"><i class="fa fa-refresh"></i> Check for update</button>
+            </div>
+        </div>
+        <div class="col-right mCustomScrollbar" style="max-height: 165px">
+            Latest changes:
+            <ul class="changes" style="text-align: left">
+                <?php
+                $changes = shell_exec('git log -20 --pretty=%B');
+
+                $changes = explode("\n\n", $changes);
+
+                array_pop($changes);
+
+                foreach ($changes as $change) {
+                    echo '<li>', $change, '</li>';
+                }
+                ?>
+            </ul>
         </div>
     </div>
 
