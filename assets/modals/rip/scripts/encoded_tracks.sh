6822894498edb930c55eb45292e24e50e142e885
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-11-26 16:23:10 +0000

    Set default return value to 0

--- assets/modals/rip/scripts/encoded_tracks.sh
+++ assets/modals/rip/scripts/encoded_tracks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # $1 is the folder name
 DIRECTORY=/var/www/html/jukebox/ripper_encoded/$1/
+TRACKS=0
+
 if [ -d "$DIRECTORY" ]; then
-  rippedTracks=$(ls -l "$DIRECTORY" | grep .*.mp3 | wc -l)
+    TRACKS=$(ls -l "$DIRECTORY" | grep .*.mp3 | wc -l)
 fi
 
-echo $rippedTracks
+echo "$TRACKS"
--- assets/modals/rip/scripts/ripped_tracks.sh
+++ assets/modals/rip/scripts/ripped_tracks.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-rippedTracks=$(ls -l /var/www/html/jukebox/cdparanoia/ | grep .*.wav | wc -l)
 
-echo $rippedTracks
+DIRECTORY=/var/www/html/jukebox/cdparanoia/
+TRACKS=0
+
+if [ -d "$DIRECTORY" ]; then
+    TRACKS=$(ls -l "$DIRECTORY" | grep .*.wav | wc -l)
+fi
+
+echo "$TRACKS"
