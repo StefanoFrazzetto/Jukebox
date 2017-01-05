5797b653e8742bee3cf60253c91a5ccf11f4b633
Vittorio <pryderi.mail@gmail.com>
2017-01-05 15:05:01 +0000

    Created increment method in Database

--- assets/php/Database.php
+++ assets/php/Database.php
@@ -1,30 +1,32 @@
 <?php
 
 /**
-*	Mysql database class
-*/
-class Database {
+ *    Mysql database class
+ */
+class Database
+{
     public static $_table_albums = "albums";
-    public static $_table_radio_stations = "radio_stations"; //The single instance
-    private static $_instance;
-	private $_connection;
-	private $_host = "localhost";
-	private $_username = "root";
-	private $_password = "password1000";
-	private $_database = "zwytytws_albums";
+    public static $_table_radio_stations = "radio_stations";
+    private static $_instance; //The single instance
+    private $_connection;
+    private $_host = "localhost";
+    private $_username = "root";
+    private $_password = "password1000";
+    private $_database = "zwytytws_albums";
 
-	/*
-	*	Get an instance of the Database
-	*	@return Instance
-	*/
+    /*
+    *	Get an instance of the Database
+    *	@return Instance
+    */
 
-	public function __construct() {
-		try {
-			$this->_connection = new PDO("mysql:host=$this->_host;dbname=$this->_database;charset=utf8mb4", $this->_username, $this->_password);
-		} catch (PDOException $e) {
-			echo 'Connection failed: ' . $e->getMessage();
-		}
-	}
+    public function __construct()
+    {
+        try {
+            $this->_connection = new PDO("mysql:host=$this->_host;dbname=$this->_database;charset=utf8mb4", $this->_username, $this->_password);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+    }
 
     // Constructor
 
@@ -34,7 +36,7 @@ class Database {
             self::$_instance = new self();
         }
         return self::$_instance;
-	}
+    }
 
     // Magic method clone is empty to prevent duplication of connection
 
@@ -44,13 +46,14 @@ class Database {
      * @param string $query
      * @return array | boolean
      */
-	public function rawQuery($query = "") {
-		if($query == "") {
-			return false;
-		}
+    public function rawQuery($query = "")
+    {
+        if ($query == "") {
+            return false;
+        }
 
-		try {
-			$stmt = $this->getConnection()->prepare($query);
+        try {
+            $stmt = $this->getConnection()->prepare($query);
             $stmt->execute();
 
             if ($stmt === false) {
@@ -58,10 +61,10 @@ class Database {
             }
 
             return $stmt->fetchAll(PDO::FETCH_NUM);
-		} catch (PDOException $e) {
-			return false;
-		}
-	}
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 
     // Get connection
 
@@ -77,25 +80,26 @@ class Database {
      * @param $array
      * @return bool
      */
-	public function insert($table, $array) {
-		$array_fields = array_keys($array);
+    public function insert($table, $array)
+    {
+        $array_fields = array_keys($array);
 
-		$fields = '(' . implode(',', $array_fields) . ')';
-		$val_holders = '(:' . implode(', :', $array_fields) . ')';
+        $fields = '(' . implode(',', $array_fields) . ')';
+        $val_holders = '(:' . implode(', :', $array_fields) . ')';
 
-		$sql = "INSERT INTO $table";
-		$sql .= $fields . ' VALUES ' . $val_holders;
+        $sql = "INSERT INTO $table";
+        $sql .= $fields . ' VALUES ' . $val_holders;
 
-		$stmt = $this->_connection;
-		$stmt = $stmt->prepare($sql);
+        $stmt = $this->_connection;
+        $stmt = $stmt->prepare($sql);
 
         foreach ($array as $key => $value) {
             $value = addslashes($value);
             $stmt->bindValue(":$key", $value);
         }
 
-		return $stmt->execute();
-	}
+        return $stmt->execute();
+    }
 
     /**
      * Returns the last inserted ID
@@ -116,29 +120,30 @@ class Database {
      *
      * @return array|null
      */
-	public function select($columns = "*", $table = "albums", $query = "WHERE 1") {
+    public function select($columns = "*", $table = "albums", $query = "WHERE 1")
+    {
         if (is_array($columns)) {
-			$columns = implode(', ', $columns);
-		}
+            $columns = implode(', ', $columns);
+        }
 
-        $sql = "SELECT $columns FROM $table";
-		$sql .= $query;
+        $sql = "SELECT $columns FROM $table ";
+        $sql .= $query;
 
-		$stmt = $this->_connection->prepare($sql);
+        $stmt = $this->_connection->prepare($sql);
 
         // DEBUG
-		// $stmt->debugDumpParams();
+        // $stmt->debugDumpParams();
 
-		$stmt->execute();
-		$rows = $stmt->fetchAll(PDO::FETCH_CLASS);
-		$row_count = $stmt->rowCount();
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
+        $row_count = $stmt->rowCount();
 
-		if($row_count == 0) {
-			return null;
-		}
+        if ($row_count == 0) {
+            return null;
+        }
 
-		return $rows;
-	}
+        return $rows;
+    }
 
     /**
      * Update a record.
@@ -148,33 +153,76 @@ class Database {
      * @param string $where
      * @return bool
      */
-	public function update($table = "", $array, $where = "") {
-		if($table == "" || $array == null) {
-			return false;
-		}
+    public function update($table = "", $array, $where = "")
+    {
+        if ($table == "" || $array == null) {
+            return false;
+        }
 
-		$sql = "UPDATE $table SET ";
+        $sql = "UPDATE $table SET ";
 
-		foreach ($array as $key => $value) {
+        foreach ($array as $key => $value) {
             $value = addslashes($value);
-			$sql .= $key . "=" . "'$value'" . ",";
-		}
+            $sql .= $key . "=" . "'$value'" . ",";
+        }
 
-		$sql = rtrim($sql, ",");
+        $sql = rtrim($sql, ",");
 
-		$sql .= " WHERE " . $where;
+        $sql .= " WHERE " . $where;
 
-		$stmt = $this->_connection;
-		$stmt = $stmt->prepare($sql);
+        $stmt = $this->_connection;
+        $stmt = $stmt->prepare($sql);
 
         foreach ($array as $key => $value)
-		    $stmt->bindValue(":$key", $value);
+            $stmt->bindValue(":$key", $value);
 
         // DEBUG
-		// $stmt->debugDumpParams();
+        // $stmt->debugDumpParams();
 
-		return $stmt->execute();
-	}
+        return $stmt->execute();
+    }
+
+
+    /**
+     * Increase one or more numeric fields.
+     * @param $table string name of the table to update
+     * @param $fields array | string field(s) to alter
+     * @param $where string clause
+     * @return bool success status
+     */
+    public function increment($table, $fields, $where = '')
+    {
+        if ($table == "" || $fields == null) {
+            return false;
+        }
+
+        $sql = "UPDATE $table SET ";
+
+        if (!is_array($fields)) {
+            $fields = [$fields];
+        }
+
+        foreach ($fields as $value) {
+            $value = addslashes($value);
+            $sql .= "$value = $value + 1,";
+        }
+
+        $sql = rtrim($sql, ",");
+
+        if ($where != '')
+            $sql .= " WHERE " . $where;
+
+        $stmt = $this->_connection;
+        $stmt = $stmt->prepare($sql);
+
+//        foreach ($array as $key => $value)
+//            $stmt->bindValue(":$key", $value);
+
+        // DEBUG
+        // $stmt->debugDumpParams();
+
+        return $stmt->execute();
+    }
 
     /**
      * Remove record(s).
@@ -183,21 +231,22 @@ class Database {
      * @param string $where
      * @return bool
      */
-	public function delete($table, $where) {
-		if($table == null || $where == null) {
-			return false;
-		}
+    public function delete($table, $where)
+    {
+        if ($table == null || $where == null) {
+            return false;
+        }
 
-		$sql = "DELETE FROM $table WHERE ";
-		$sql .= $where;
+        $sql = "DELETE FROM $table WHERE ";
+        $sql .= $where;
 
-		try {
-			$stmt = $this->getConnection()->prepare($sql);
-			return $stmt->execute();
-		} catch (PDOException $e) {
-			return false;
-		}
-	}
+        try {
+            $stmt = $this->getConnection()->prepare($sql);
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 
     /**
      * Drop one or more tables.
@@ -205,29 +254,30 @@ class Database {
      * @param string $tables
      * @return bool
      */
-	public function drop($tables = "") {
+    public function drop($tables = "")
+    {
 
-		switch($tables) {
-			case "albums":
-				$sql = "TRUNCATE TABLE " . self::$_table_albums;
-				break;
-			case "radio_stations":
-				$sql = "TRUNCATE TABLE " . self::$_table_radio_stations;
-				break;
-			default:
-				$sql = "TRUNCATE TABLE " . self::$_table_albums . "; TRUNCATE TABLE " . self::$_table_radio_stations;
-		}
+        switch ($tables) {
+            case "albums":
+                $sql = "TRUNCATE TABLE " . self::$_table_albums;
+                break;
+            case "radio_stations":
+                $sql = "TRUNCATE TABLE " . self::$_table_radio_stations;
+                break;
+            default:
+                $sql = "TRUNCATE TABLE " . self::$_table_albums . "; TRUNCATE TABLE " . self::$_table_radio_stations;
+        }
 
         try {
-			$stmt = $this->getConnection()->prepare($sql);
-			return $stmt->execute();
-		} catch (PDOException $e) {
-			return false;
-		}
+            $stmt = $this->getConnection()->prepare($sql);
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
     }
 
     private function __clone()
     {
-	}
+    }
 
 }
