d096fbf63b3b0f9dbe2dbe0f2d504fc60d98c08e
Vittorio <pryderi.mail@gmail.com>
2017-01-18 10:28:26 +0000

    Improved database migration script

--- assets/php/update_legacy_albums.php
+++ assets/php/update_legacy_albums.php
@@ -1,4 +1,4 @@
-<?php
+<pre><?php
 /**
  * Used to update the legacy database to the new one
  * Created by PhpStorm.
@@ -7,11 +7,25 @@
  * Time: 11:30
  */
 
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
 include_once '../php-lib/MusicClasses/Album.php';
 
-foreach (Album::getAllAlbums() as $album)
-    foreach ($album->getLegacySongs() as $song) {
-        echo json_encode($song);
+echo "<h1>[@] Starting migration...</h1>", PHP_EOL;
+
+foreach (Album::getAllAlbums() as $album) {
+    echo '<h2>', $album->getTitle(), '</h2><ul>';
+    foreach ($album->getLegacySongs() as $track_no => $song) {
+        if ($song->getTrackNo() == 0)
+            $song->setTrackNo($track_no + 1);
+
         $song->save();
         $song->addArtist($album->getLegacyArtist()->getId());
+        echo "<li>", json_encode($song), "</li>";
     }
+    echo '</ul>';
+}
+
+echo PHP_EOL, "<h1>[@] Migration Complete.</h1>";
