4cc279ef7829f82f11d1c05ad258288158162e90
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-02-07 21:42:48 +0000

    Fix ripping script

--- assets/cmd/disc/rip_handler.sh
+++ assets/cmd/disc/rip_handler.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-# Set this script directory
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source ${THIS_DIR}/../functions.sh
-source ${THIS_DIR}/disc_statuses.sh
-
 DEVICE="$device"
 CDPARANOIA_LOG_PATH="$cdparanoia_log_path"
 LAME_LOG_PATH="$lame_log_path"
 RIPPING_DIR="$ripping_dir"
 ENCODING_DIR="$encoding_dir"
-
-rmDir "$RIPPING_DIR"
-mkdir -p "$RIPPING_DIR"
-
-rmdir "$ENCODING_DIR"
-mkdir -p "$ENCODING_DIR"
-
-mkdir -p "$(dirname "$CDPARANOIA_LOG_PATH")"
-mkdir -p "$(dirname "$LAME_LOG_PATH")"
 
 ## START THE RIPPING PROCESS
 #
@@ -41,11 +26,11 @@ mkdir -p "$(dirname "$LAME_LOG_PATH")"
 # 1:-
 #   Encode the entire disc from the first track.
 
-cdparanoia -vB -X -d ${DEVICE} 1:- ${RIPPING_DIR}/ >> ${CDPARANOIA_LOG_PATH} 2>&1
+cdparanoia -vB -X -d ${DEVICE} 1:- ${RIPPING_DIR} >> ${CDPARANOIA_LOG_PATH} 2>&1
 
 
 ## GET THE RIPPER TRACKS
-TRACKS=$(find ${RIPPING_DIR}/ -type f -name "*.wav")
+TRACKS=$(find ${RIPPING_DIR} -type f -name "*.wav")
 
 ## ENCODE TRACK BY TRACK
 for track in ${TRACKS};
