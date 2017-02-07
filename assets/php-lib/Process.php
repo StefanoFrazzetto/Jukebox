1da2cda27c4cd6c3f442e1f7ddfb3ea9a8c2af08
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-02-07 14:22:06 +0000

    Improve phpdoc

--- assets/php-lib/Process.php
+++ assets/php-lib/Process.php
@@ -9,41 +9,85 @@
 
 class Process
 {
+    /** @var  int The process id */
     private $pid;
+
+    /** @var string The command or path to script to execute */
     private $command;
+
+    /** @var string The path to the file where the output of the process will be saved */
     private $output_file;
 
-    public function __construct($cl = false, $output_file = "/dev/null")
+    /**
+     * Create a new instance of a process.
+     *
+     * If the first parameters is not empty, the process will be spawned.
+     * If the second parameter is not empty, it will be used as output file.
+     *
+     * @param string $cl The command or path to script to execute.
+     * @param string $output_file The path to the file where the output of the
+     * process will be saved.
+     */
+    public function __construct($cl = "", $output_file = "/dev/null")
     {
-        if ($cl != false) {
+        if (!empty($cl)) {
             $this->command = $cl;
+            $this->output_file = $output_file;
             $this->runCom();
         }
     }
 
+    /**
+     * Run the command or script and set the process id of the new process.
+     */
     private function runCom()
     {
-        $command = 'nohup ' . $this->command . " > $this->output_file 2>&1 & echo \$!";
-        exec($command, $op);
+        $command = 'nohup bash ' . $this->command . " > $this->output_file 2>&1 & echo \$!";
+        exec("$command", $op);
         $this->pid = (int)$op[0];
+        return true;
     }
 
+    /**
+     * Return the process id of the process.
+     *
+     * @return int The process id of the process.
+     */
     public function getPid()
     {
         return $this->pid;
     }
 
+    /**
+     * Set the process id.
+     *
+     * @param int $pid The process id.
+     */
     public function setPid($pid)
     {
         $this->pid = $pid;
     }
 
+    /**
+     * Start the process.
+     *
+     * @return bool true if the process is started successfully,
+     * false otherwise.
+     */
     public function start()
     {
-        if ($this->command != '') $this->runCom();
-        else return true;
+        if ($this->command != '') {
+            return $this->runCom();
+        } else {
+            return false;
+        }
     }
 
+    /**
+     * Stop the process.
+     *
+     * @return bool true if the process is stopped, false otherwise.
+     */
     public function stop()
     {
         $command = 'kill ' . $this->pid;
@@ -52,6 +96,11 @@ class Process
         else return false;
     }
 
+    /**
+     * Return the status of the process.
+     *
+     * @return bool true if the process is running, false otherwise.
+     */
     public function status()
     {
         $command = 'ps -p ' . $this->pid;
