cfd35979d69d84248dbd4df47c3683abe343e4c1
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-02-11 17:53:17 +0000

    Create FileUtils tests

--- tests/FileUtilsTest.php
+++ tests/FileUtilsTest.php
@@ -1,8 +1,12 @@
 <?php
 
 require_once __DIR__ . '/JukeboxTest.php';
+use Lib\FileUtils;
 
 final class FileUtilsTest extends JukeboxTest
 {
-
+    public function testCreateFile()
+    {
+        $this->assertTrue(FileUtils::createFile("test.txt"), 'Cannot create file.');
+    }
 }
