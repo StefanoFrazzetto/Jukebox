8b68ddbec83d9b896ff9e4d451daf8578496aa78
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-02-11 17:53:39 +0000

    Create jukebox test class

--- tests/JukeboxTest.php
+++ tests/JukeboxTest.php
@@ -6,5 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class JukeboxTest extends TestCase
 {
+    /**
+     * @var string the directory where test files can be created.
+     */
+    const TEST_DIR = '/tmp/tests/';
 
+    /**
+     * Create the tests directory.
+     */
+    public static function setUpBeforeClass()
+    {
+        mkdir(self::TEST_DIR, 0777);
+    }
+
+    /**
+     * Remove the tests directory and all the files/folder it contains.
+     */
+    public static function tearDownAfterClass()
+    {
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(self::TEST_DIR, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($files as $fileinfo) {
+            $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+            $todo($fileinfo->getRealPath());
+        }
+
+        rmdir(self::TEST_DIR);
+    }
 }
