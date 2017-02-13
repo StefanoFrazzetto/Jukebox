1876cf44c90d5b6eee2e9116cfcdfbbc9759f8f0
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-02-13 16:21:10 +0000

    Check if the file was already opened

--- assets/php/startup_scripts.php
+++ assets/php/startup_scripts.php
@@ -6,17 +6,21 @@
 use Lib\Network;
 use Lib\Speakers;
 
-$network = new Network();
+if (!isset($_SESSION['started']) || !$_SESSION['started']) {
+    $_SESSION['started'] = true;
 
-if (!$network->isConnected()) {
-    try {
-        $network->load_network();
-        $network->connect();
-    } catch (Exception $e) {
-        error_log("Failed to connect at start up because of this: " . $e->getMessage());
-        error_log("Don't blame if it doesn't work, I am a good program, and I catch my exceptions.");
+    $network = new Network();
+
+    if (!$network->isConnected()) {
+        try {
+            $network->load_network();
+            $network->connect();
+        } catch (Exception $e) {
+            error_log("Failed to connect at start up because of this: " . $e->getMessage());
+            error_log("Don't blame if it doesn't work, I am a good program, and I catch my exceptions.");
+        }
     }
-}
 
-// Turn on the speakers
-Speakers::turnOn();
+    // Turn on the speakers
+    Speakers::turnOn();
+}
