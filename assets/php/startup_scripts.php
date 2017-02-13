ba50f0cbeaf976759601ed39afc3a55112fdd9a6
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-02-13 13:21:22 +0000

    Use class Speakers and put connection check

--- assets/php/startup_scripts.php
+++ assets/php/startup_scripts.php
@@ -3,17 +3,20 @@
 // Put all the stuff you want to execute at startup time here.
 // Example: create config folders, files, etc.
 
-require_once '../../vendor/autoload.php';
-
 use Lib\Network;
+use Lib\Speakers;
 
 $network = new Network();
 
-$network->load_network();
-
-try {
-    $network->connect();
-} catch (Exception $e) {
-    error_log("Failed to connect at start up because of this: " . $e->getMessage());
-    error_log("Don't blame if it doesn't work, I am a good program, and I catch my exceptions.");
+if (!$network->isConnected()) {
+    try {
+        $network->load_network();
+        $network->connect();
+    } catch (Exception $e) {
+        error_log("Failed to connect at start up because of this: " . $e->getMessage());
+        error_log("Don't blame if it doesn't work, I am a good program, and I catch my exceptions.");
+    }
 }
+
+// Turn on the speakers
+Speakers::turnOn();
