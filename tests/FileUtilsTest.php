9f44d355dfd8df14dd2ebfec827f94c667b6c28d
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-03-06 20:11:44 +0000

    Move files and directory creation to FileUtils

--- tests/FileUtilsTest.php
+++ tests/FileUtilsTest.php
@@ -1,10 +1,47 @@
 <?php
 
-require_once __DIR__.'/JukeboxTestClass.php';
+require_once 'JukeboxTestClass.php';
+
 use Lib\FileUtils;
 
 final class FileUtilsTest extends JukeboxTestClass
 {
+    /**
+     * @var string the directory where test files can be created.
+     */
+    const TEST_DIR = '/tmp/tests/';
+
+    /**
+     * @var string the path to a file that can be used for tests.
+     */
+    const TEST_FILE = '/tmp/tests/test.txt';
+
+    /**
+     * Create the tests directory.
+     */
+    public static function setUpBeforeClass()
+    {
+        mkdir(self::TEST_DIR, 0777);
+    }
+
+    /**
+     * Remove the tests directory and all the files/folder it contains.
+     */
+    public static function tearDownAfterClass()
+    {
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(self::TEST_DIR, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($files as $fileinfo) {
+            $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+            $todo($fileinfo->getRealPath());
+        }
+
+        rmdir(self::TEST_DIR);
+    }
+
     public function testCreateFile()
     {
         $this->assertTrue(FileUtils::createFile(self::TEST_FILE), 'Cannot create file.');
--- tests/JukeboxTestClass.php
+++ tests/JukeboxTestClass.php
@@ -6,39 +6,5 @@ use PHPUnit\Framework\TestCase;
 
 class JukeboxTestClass extends TestCase
 {
-    /**
-     * @var string the directory where test files can be created.
-     */
-    const TEST_DIR = '/tmp/tests/';
 
-    /**
-     * @var string the path to a file that can be used for tests.
-     */
-    const TEST_FILE = '/tmp/tests/test.txt';
-
-    /**
-     * Create the tests directory.
-     */
-    public static function setUpBeforeClass()
-    {
-        mkdir(self::TEST_DIR, 0777);
-    }
-
-    /**
-     * Remove the tests directory and all the files/folder it contains.
-     */
-    public static function tearDownAfterClass()
-    {
-        $files = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator(self::TEST_DIR, RecursiveDirectoryIterator::SKIP_DOTS),
-            RecursiveIteratorIterator::CHILD_FIRST
-        );
-
-        foreach ($files as $fileinfo) {
-            $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
-            $todo($fileinfo->getRealPath());
-        }
-
-        rmdir(self::TEST_DIR);
-    }
 }
