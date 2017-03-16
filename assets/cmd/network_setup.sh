68a5f4f5523739597cdf8071c587a3b61282c07b
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-03-16 21:05:57 +0000

    Fix network script It did not work in Wi-Fi mode. At least one DNS should be always passed by the modal, but since it's not, Google DNS has been hard-coded to avoid errors.

--- assets/cmd/network_setup.sh
+++ assets/cmd/network_setup.sh
@@ -8,8 +8,9 @@ DNS_CONF=/etc/resolv.conf
 LOG_FILE=/var/www/html/logs/connection-script.log
 
 # Logs the message to the log file
+echo -e "\n\n" >> "$LOG_FILE"
 logMessage() {
-    echo -e "[$(date --rfc-3339='ns')] $1" >> "$LOG_FILE"
+    echo -e "[$(date --rfc-3339='ns')]: $1" >> "$LOG_FILE"
 }
 
 logMessage "Network script started."
@@ -42,6 +43,9 @@ writeConfig() {
     # $3 IP address
     # $4 Netmask
     # $5 Gateway
+    # $6 Main DNS IP
+
+    logMessage "Writing config to $INTERFACE_FILE: $1 $2 $3 $4 $5 $6"
 
     # Release the dhcp and bring the interface down
     stopInterface $1
@@ -49,28 +53,39 @@ writeConfig() {
     echo -e "auto lo\niface lo inet loopback\n" > "$INTERFACE_FILE"
 
     # Static IP
-    if [ $2 == "dhcp" ]; then
+    if [ "$2" = "dhcp" ]; then
         # DHCP
         echo -e "auto $1
         iface $1 inet dhcp" >> "$INTERFACE_FILE"
 
-        sudo ifconfig $1 up
-        sudo dhclient $1
+        # Set the DNS servers
+        setDns "8.8.8.8"
+
+        sudo ifconfig "$1" up
+        sudo dhclient "$1"
     else
         # Check the number of arguments
-        if [ $# != 5 ]; then
-            echo -e "Script Requires at-least 5 Arguments For Static IP Settings."
+        if [ $# != 6 ]; then
+            echo -e "This script REQUIRES 6 Arguments For STATIC IP Settings."
             exit 1
         fi
 
         # Write the configuration
         echo -e "auto $1\niface $1 inet static\n\taddress $3\n\tnetmask $4\n\tgateway $5" >> "$INTERFACE_FILE"
+
+        # Set the DNS servers
+        setDns "$6"
+
+        sudo ifconfig "$1" up
+        logMessage "Setting configuration: $1 $3 $4 $5"
+        setConfiguration "$1" "$3" "$4" "$5"
     fi
 }
 
 setDns() {
-    echo "nameserver $1
-    nameserver $2" > "$DNS_CONF"
+    second_dns="8.8.4.4"
+    logMessage "Setting DNS: $1 - $second_dns"
+    echo -e "nameserver $1\nnameserver $second_dns" > "$DNS_CONF"
 }
 
 # Sets the wifi password
@@ -82,16 +97,17 @@ setWifiPassword() {
     stopInterface "$wifi_interface"
     killall wpa_supplicant
 
-    if [ $1 == "wpa" ]; then
+    if [ "$1" = "wpa" ]; then
+        sudo echo '[Network_script] wpa-passphrase:' >> /var/log/apache2/error.log
         wpa_passphrase "$2" "$3" > "$WPA_SUPPLICANT"
         echo -e "\twpa-essid $2" >> "$INTERFACE_FILE";
         echo -e "\twpa-psk $3" >> "$INTERFACE_FILE";
 
-    elif [ $1 == "wep" ]; then
+    elif [ "$1" = "wep" ]; then
         wpa_passphrase "$2" "$3" > "$WPA_SUPPLICANT"
         echo -e "\twireless-essid $2" >> "$INTERFACE_FILE";
         echo -e "\twireless-key \"$3\"" >> "$INTERFACE_FILE";
-    elif [ $1 == "open" ]; then
+    elif [ "$1" = "open" ]; then
         echo "network={" > "$WPA_SUPPLICANT"
         echo -e "\tssid=\"$2\"" >> "$WPA_SUPPLICANT"
         echo -e "\tkey_mgmt=NONE" >> "$WPA_SUPPLICANT"
@@ -99,21 +115,20 @@ setWifiPassword() {
         echo "}" >> "$WPA_SUPPLICANT"
 	fi
 
-    sudo ifconfig "$wifi_interface" up
-
-	if [ $1 == "open" ]; then
-         sudo wpa_supplicant -B -i $wifi_interface -c "$WPA_SUPPLICANT" -Dwext &
+	if [ "$1" = "open" ]; then
+         sudo wpa_supplicant -B -i"$wifi_interface" -c"$WPA_SUPPLICANT" -Dwext &
     else
-         sudo wpa_supplicant -Dwext -i$wifi_interface -c"$WPA_SUPPLICANT" &
+         sudo echo '[Network_script] wpa-supplicant:' >> /var/log/apache2/error.log
+         sudo wpa_supplicant -Dwext -i"$wifi_interface" -c"$WPA_SUPPLICANT" &
          sleep 3
     fi
 }
 
 
 
-if [ $1 == "none" ]; then
+if [ "$1" = "none" ]; then
     killall wpa_supplicant
-    sudo dhclient -r $wifi_interface
+    sudo dhclient -r "$wifi_interface"
     sudo dhclient -r eth0
     sudo hap_app stop
     echo "" > "$INTERFACE_FILE"
@@ -130,28 +145,19 @@ gateway=$5	    #192.168.1.1
 dns=$6		    #8.8.8.8
 
 # LAN
-if [ $network == "ethernet" ]; then
+if [ "$network" = "ethernet" ]; then
     logMessage "Starting ethernet connection."
 
     logMessage "Stopping Wi-Fi interface: $wifi_interface."
     stopInterface "$wifi_interface"
 
     # Write the configuration to the interfaces file
-    logMessage "Writing ethernet configuration. Parameters:"
-    logMessage "$1 $2 $3 $4 $5 $6"
-    writeConfig "eth0" "$dhcp" "$ip_address" "$netmask" "$gateway"
-
-    # Set the DNS servers
-    logMessage "Setting DNS: $dns"
-    setDns "8.8.8.8" "8.8.4.4"
-
-    sudo ifconfig eth0 up
-    logMessage "Setting configuration: eth0 $ip_address $netmask $gateway"
-    setConfiguration "eth0" "$ip_address" "$netmask" "$gateway"
+    logMessage "Writing ethernet configuration. Parameters: $network $dhcp $ip_address $netmask $gateway $dns"
+    writeConfig "eth0" "$dhcp" "$ip_address" "$netmask" "$gateway" "$dns"
 fi	
 
 # WIFI
-if [ $network == "wifi" ]; then
+if [ "$network" = "wifi" ]; then
  	network_name=$7
  	password=$8
  	wifi_encryption=$9	#wpa
@@ -160,25 +166,16 @@ if [ $network == "wifi" ]; then
     logMessage "Stopping ethernet interface: eth0."
     stopInterface "eth0"
 
-    # Write the configuration to the interfaces file
-    logMessage "Writing Wi-Fi configuration. Parameters:"
-    logMessage "$1 $2 $3 $4 $5 $6"
-    writeConfig "$wifi_interface" "$dhcp" "$ip_address" "$netmask" "$gateway"
-
     # Set wifi password
     setWifiPassword "$wifi_encryption" "$network_name" "$password"
 
-    # Set DNS servers
-    logMessage "Setting DNS: $dns"
-    setDns "8.8.8.8" "8.8.4.4"
-
-    sudo ifconfig "$wifi_interface" up
-    logMessage "Setting configuration: $wifi_interface $ip_address $netmask $gateway"
-    setConfiguration "$wifi_interface" "$ip_address" "$netmask" "$gateway"
+    # Write the configuration to the interfaces file
+    logMessage "Writing ethernet configuration. Parameters: $network $dhcp $ip_address $netmask $gateway $dns"
+    writeConfig "$wifi_interface" "$dhcp" "$ip_address" "$netmask" "$gateway" "$dns"
 fi
 
 # HOTSPOT
-if [ $network == "hotspot" ]; then
+if [ "$network" = "hotspot" ]; then
     logMessage "Starting hotspot"
 
     echo "" > "$INTERFACE_FILE"
