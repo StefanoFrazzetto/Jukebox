380c0ad3a2447fd7dde219217ca5fba0acf13cd2
Vittorio <pryderi.mail@gmail.com>
2017-04-08 17:07:36 +0100

    First (unstable) iteration of the new rip UI

--- assets/modals/album_upload/uploaders/rip.php
+++ assets/modals/album_upload/uploaders/rip.php
@@ -1,9 +1,29 @@
 <div class="modalHeader">Album Uploader</div>
 <div class="modalBody center">
-    <button id="ripperCheckStatus">Check</button>
-    <button id="ripperStart">Start!</button>
-    <button id="ripperStop">Stop.</button>
+
+    <p>You can import an audio CD to the Jukebox using this feature.</p>
+
+    <div id="ripperMain" style="margin: 50px">
+        <button id="ripperStart">Start</button>
+        <button id="ripperStop" class="hidden">Abort</button>
+
+        <div id="ripperSuccess" class="hidden">
+            <p>CD imported successfully!</p>
+
+            <p>
+                <button id="ripperReset">Reset</button>
+            </p>
+
+            <!--<button id="ripperCheckStatus">Check</button>-->
+        </div>
+    </div>
+
+    <hr/>
+    <div class="progressBar full-wide" style="margin: 0">
+        <div class="progress" id="ripperProgress"></div>
+    </div>
 </div>
+
 <div class="modalFooter">
     <button id="btnBack">Back</button>
     <button id="btnCancel">Cancel</button>
@@ -18,6 +38,67 @@
         var ripperCheckStatus = $('#ripperCheckStatus');
         var ripperStart = $('#ripperStart');
         var ripperStop = $('#ripperStop');
+        var ripperSuccess = $('#ripperSuccess');
+        var ripperReset = $('#ripperReset');
+
+        var ripperProgress = $('#ripperProgress');
+
+        function setProgressPercentage(p) {
+            ripperProgress.css('width', p + '%');
+            ripperProgress.text(p + '%');
+        }
+
+        function updateStatus() {
+            getStatus(function (status, percentage) {
+                if (status === "ripping" || status === "encoding")
+                    setProgressPercentage(percentage);
+                else if (status === "complete") {
+                    alert("Process complete!");
+                    success();
+                } else if (status === "idle") {
+                    alert("The ripper is in idle...");
+                } else {
+                    error("Unknown status returned by the ripper");
+                }
+            });
+        }
+
+        var handler;
+
+        function startProgress() {
+            ripperStart.hide();
+            ripperStop.show();
+            ripperSuccess.hide();
+
+            updateStatus();
+            handler = setInterval(function () {
+                updateStatus();
+            }, 3000);
+        }
+
+        function stopProgress() {
+            clearInterval(handler);
+        }
+
+        function success() {
+            ripperStart.hide();
+            ripperStop.hide();
+            ripperSuccess.show();
+            btnNext.removeClass("disabled");
+            setProgressPercentage(100);
+            stopProgress();
+        }
+
+        function getStatus(callback) {
+            $.getJSON('/assets/API/uploader.php?action=get_ripper_status')
+                .done(function (data) {
+                    callback(data.status, data.percentage);
+                })
+                .fail(function () {
+                    error("Failed to get ripper status.");
+                    stopProgress();
+                });
+        }
 
         btnBack.click(function () {
             uploader.previousPage();
@@ -27,11 +108,11 @@
             uploader.nextPage();
         });
 
+        // CHECK
         ripperCheckStatus.click(function () {
             $.getJSON('/assets/API/uploader.php?action=get_ripper_status')
                 .done(function (data) {
                     alert(JSON.stringify(data));
-                    console.log(data);
                 })
                 .fail(function () {
                     error("Failed to check ripper status.");
@@ -39,11 +120,17 @@
 
         });
 
+        // START
         ripperStart.click(function () {
             $.getJSON('/assets/API/uploader.php?action=start_ripping&uploader_id=' + uploader.uploaderID)
                 .done(function (data) {
-                    alert(data.status);
-                    console.log(data);
+                    if (data.status === "success") {
+                        startProgress();
+                    } else {
+                        error("Failed to start the ripper. " + data.message);
+                        console.error(data);
+                    }
+
                 })
                 .fail(function () {
                     error("Failed to start ripping.");
@@ -51,15 +138,49 @@
 
         });
 
+        // STOP
         ripperStop.click(function () {
             $.getJSON('/assets/API/uploader.php?action=stop_ripping')
                 .done(function (data) {
-                    alert(data.status);
                     console.log(data);
+                    if (data.status !== "error") {
+                        alert(data.status);
+                        stopProgress()
+                    } else {
+                        error("Failed to abort the process.");
+                        //reset();
+                    }
                 })
                 .fail(function () {
                     error("Failed to stop ripping.");
                 });
-        })
+        });
+
+        function reset() {
+            $.getJSON('/assets/API/uploader.php?action=reset_ripping')
+                .done(function () {
+                    ripperStart.show();
+                    ripperStop.hide();
+                    ripperSuccess.hide();
+                    setProgressPercentage(0);
+                });
+        }
+
+        // RESET
+        ripperReset.click(function () {
+            reset();
+        });
+
+        modal.onModalClosed = function () {
+            stopProgress();
+        };
+
+        getStatus(function (status) {
+            if (status === "complete") {
+                success();
+            } else if (status === "ripping" || status === "encoding") {
+                startProgress();
+            }
+        });
     });
 </script>
