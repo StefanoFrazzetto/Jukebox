00b74ef5cbe9f8ee0aadd725f751ca2a2fcd618e
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-06-22 03:34:44 +0100

    Implement Device class in format modal
    Signed-off-by: Stefano Frazzetto <s.frazzetto22@gmail.com>

--- assets/API/format.php
+++ assets/API/format.php
@@ -2,30 +2,25 @@
 
 require_once '../../vendor/autoload.php';
 
-use Lib\Database;
-use Lib\FileUtils;
+use Lib\Device;
 
-$Database = new Database();
 
 if (isset($_GET['operation'])) {
-    $jukebox_folder = '/var/www/html/jukebox';
-    $config_folder = '/var/www/html/assets/config';
     $fail = 'Error! It was not possible to perform the requested action.';
 
     switch ($_GET['operation']) {
         case 'format_hdd_database':
-            $db_res = $Database->truncate('all');
-            FileUtils::emptyDirectory($jukebox_folder);
-            $success = 'The albums and the radio stations have been successfully removed.';
-            echo FileUtils::isDirEmpty($jukebox_folder) && $db_res ? $success : $fail;
+            $device = new Device();
+            $success_soft = 'The albums and the radio stations have been successfully removed.';
+
+            echo $device->softReset() ? $success_soft : $fail;
             break;
 
         case 'factory_reset':
-            $Database::resetDatabase();
-            FileUtils::emptyDirectory($jukebox_folder);
-            // TODO Create method to reset the configuration
-            $res = FileUtils::isDirEmpty($jukebox_folder);
-            echo $res ? 'The Jukebox has been reset to the factory settings.' : $fail;
+            $device = new Device();
+            $success_hard = 'The Jukebox has been reset to the factory settings.';
+
+            echo $device->hardReset() ? $success_hard : $fail;
             break;
 
         default:
