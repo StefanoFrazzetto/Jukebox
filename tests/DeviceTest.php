f2eb2f9c5794b0b710603f5760b4f3e8503351cf
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-06-22 03:11:21 +0100

    Add Device class tests
    Signed-off-by: Stefano Frazzetto <s.frazzetto22@gmail.com>

---
+++ tests/DeviceTest.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once 'JukeboxTestClass.php';
+
+use Lib\Device;
+
+final class DeviceTest extends JukeboxTestClass
+{
+    /** @var  Device $device the device class instance */
+    private $device;
+
+    public function setUp()
+    {
+        $this->device = new Device();
+    }
+
+    public function testFixApacheIni()
+    {
+        $method = $this->getPrivateMethod('fixApacheConfig');
+
+        $this->assertTrue($method->invokeArgs($this->device, []));
+    }
+
+    public function testFixPermissions()
+    {
+        $method = $this->getPrivateMethod('fixPermissions');
+
+        $this->assertTrue($method->invokeArgs($this->device, []));
+    }
+
+    public function testSoftReset()
+    {
+        $this->assertTrue($this->device->softReset());
+    }
+
+    public function testHardReset()
+    {
+        $this->assertTrue($this->device->hardReset());
+    }
+
+    /**
+     * Return a private method making it public.
+     *
+     * @param string $name the method name
+     * @return ReflectionMethod the method
+     */
+    private static function getPrivateMethod($name) {
+        $class = new ReflectionClass(Device::class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+}
