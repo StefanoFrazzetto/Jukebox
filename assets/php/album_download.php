338794f60260275c510b2e40471c0498317dfb4f
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-08-04 19:51:39 +0200

    Improve album download using refactored Zipper class

--- assets/php/album_download.php
+++ assets/php/album_download.php
@@ -1,70 +1,19 @@
 <?php
 
 require_once '../../vendor/autoload.php';
-require_once '../php-lib/Zipper.php';
 
-use Lib\MusicClasses\Album;
-use Lib\MusicClasses\Song;
 
-$outputDIR = '/var/www/html/downloads/';
-$zipCheck = $outputDIR.'zipCheck';
+use Lib\Zipper;
 
 $albumID = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$output = '';
 
-if (file_exists($zipCheck) && (time() - file_get_contents($zipCheck)) > 300) {
-    unlink($zipCheck);
-}
-
-if (!file_exists($zipCheck)) {
-    $Album = Album::getAlbum($albumID);
-    $Songs = Song::getSongsInAlbum($albumID);
-
-    $title = $Album->getTitle();
-    $artists = $Album->getArtistsName();
-
-    $outputFileName = preg_replace('/[^A-Za-z0-9\-]/', ' ', implode($artists, '-')).' - '.preg_replace('/[^A-Za-z0-9\-]/', ' ', $title);
-    $outputFile = $outputDIR.$outputFileName.'.zip';
-
-    if (!file_exists($outputDIR)) {
-        mkdir($outputDIR, 0755, true);
-    }
-    file_put_contents($zipCheck, time());
-
-    if (end($Songs)->getCd() != $Songs[0]->getCd()) {
-        $differentCDs = true;
-        $CD = -1; //Del
-    } else {
-        $differentCDs = false;
-    }
-
-    foreach ($Songs as $key => $song) {
-        if ($differentCDs) {
-            if ($song->getCd() != $CD) {
-                $CD = $song->getCd();
-                //echo $CD;
-            }
-            $albumMap[$CD][] = $song->getUrl();
-        } else {
-            $CD = $song->getCd();
-            $albumMap[$CD][] = $song->getUrl();
-        }
-    }
-
-    if (!file_exists($outputFile) && isset($albumMap)) {
-        Zipper::zip($albumMap, $outputFileName, $albumID);
-    }
-
-    while (!file_exists($outputFile)) {
-        sleep(1);
-    }
-
-    $outputFile = 'http://'.$_SERVER['HTTP_HOST'].'/downloads/'.$outputFileName.'.zip';
-
-    $output = "<br/><a href='".$outputFile."'><button>Download Album</button></a>";
-
-    unlink($zipCheck);
-} else {
-    $output = 'You\'re already downloading an album. Try again later.';
+try {
+    $zipper = new Zipper($albumID);
+    $zipper->createZip();
+    $output = "<br/><a href='" . $zipper->getDownloadURL() . "'><button>Download Album</button></a>";
+} catch (Exception $e) {
+    $output = $e->getMessage();
 }
 
 echo $output;
