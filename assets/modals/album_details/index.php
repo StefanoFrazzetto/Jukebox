95a3e6491873f67fea63255aafe2a7f39537d92d
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-08-25 15:11:58 +0200

    Album song table updated with the new styling

--- assets/modals/album_details/index.php
+++ assets/modals/album_details/index.php
@@ -35,55 +35,57 @@ foreach ($artists_ids as $artist_id) {
         <input type="hidden" value="<?php echo $album->getId() ?>" id="currentAlbumId">
         <img id="album_cover_img" src="<?php echo $album->getCoverUrl() ?>"/>
 
-        <table class="mCustomScrollbar songsTable" id="albumDetails" data-album="<?php echo $album->getId() ?>">
-            <tr class="th">
-                <th>#</th>
-                <th class="playlist_title">Title</th>
-                <th>Length</th>
-            </tr>
-            <?php
-
-            $tracks = $album->getSongs();
-
-            $differentCDs = $album->getCdCount() != 1;
-            $CD = -1;
-            $CDMap = [];
-
-            if (is_array($tracks) && count($tracks) > 0) {
-                foreach ($tracks as $key => $track) {
-                    if ($differentCDs) {
-                        if ($track->getCd() != $CD) {
-                            $CD = $track->getCd();
-                            echo "<tr class='th'><th colspan='3' data-cd='", $CD, "' class='CDth'>CD ", $CD, "<span class='addTrackToPlaylist'>+</span></th></tr>";
-                        }
-
-                        $CDMap[$CD][] = $key;
-                    } ?>
-                    <tr class="trackRow"
-                        data-track-id="<?php echo $track->getId() ?>"
-                        data-track-no="<?php echo $key + 1; ?>"
-                        data-album="<?php echo $albumID ?>">
-                        <td class="num">
-                            <?php echo $key + 1; ?>
-                        </td>
-                        <td class="playlist_title">
-                            <?php echo $track->getTitle(); ?>
-                            <span class="addTrackToPlaylist">+</span>
-                        </td>
-                        <td class="duration">
-                            <?php echo $track->getTimeString(); ?>
-                        </td>
-                    </tr>
-                    <?php
-                }
-            } else {
-                ?>
+        <div class="mCustomScrollbar tableContainer">
+            <table class=" cooltable small" id="albumDetails" data-album="<?php echo $album->getId() ?>">
                 <tr class="th">
-                    <th colspan="3">No tracks found</th>
+                    <th>#</th>
+                    <th class="playlist_title">Title</th>
+                    <th>Length</th>
                 </tr>
                 <?php
-            } ?>
-        </table>
+
+                $tracks = $album->getSongs();
+
+                $differentCDs = $album->getCdCount() != 1;
+                $CD = -1;
+                $CDMap = [];
+
+                if (is_array($tracks) && count($tracks) > 0) {
+                    foreach ($tracks as $key => $track) {
+                        if ($differentCDs) {
+                            if ($track->getCd() != $CD) {
+                                $CD = $track->getCd();
+                                echo "<tr class='th'><th colspan='3' data-cd='", $CD, "' class='CDth'>CD ", $CD, "<span class='addTrackToPlaylist'>+</span></th></tr>";
+                            }
+
+                            $CDMap[$CD][] = $key;
+                        } ?>
+                        <tr class="trackRow"
+                            data-track-id="<?php echo $track->getId() ?>"
+                            data-track-no="<?php echo $key + 1; ?>"
+                            data-album="<?php echo $albumID ?>">
+                            <td class="num">
+                                <?php echo $key + 1; ?>
+                            </td>
+                            <td class="playlist_title">
+                                <?php echo $track->getTitle(); ?>
+                                <span class="addTrackToPlaylist">+</span>
+                            </td>
+                            <td class="duration">
+                                <?php echo $track->getTimeString(); ?>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                } else {
+                    ?>
+                    <tr class="th">
+                        <th colspan="3">No tracks found</th>
+                    </tr>
+                    <?php
+                } ?>
+            </table>
+        </div>
         <?php
         if ($differentCDs) { // This will inject the CD/tracks map  as a javascript variable. I don't know whether it's nasty or cool.
             echo '<script>',
