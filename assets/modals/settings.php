6b5101db681abec920f7d41a3c5baa41c81fa9e0
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-10-01 14:47:57 +0100

    Modernised settings home

--- assets/modals/settings.php
+++ assets/modals/settings.php
@@ -7,50 +7,99 @@ use Lib\Speakers;
 ?>
 
 <div class="modalHeader">Settings</div>
-<div class="modalBody mCustomScrollbar" data-mcs-theme="dark">
-    <div id="buttonSettings" style="text-align: center; line-height: 39px;">
-        Speakers
+
+<div class="modalBody mCustomScrollbar text-center settings-home" data-mcs-theme="dark">
+    <div class="selector eject">
+        <i class="fa fa-5x fa-eject"></i>
+        <p>Eject Disc</p>
+    </div>
+
+    <div class="selector speakers">
+        <i class="fa fa-5x fa-volume-up"></i>
+        <p></p>
         <div class="onoffswitch inline" id="speakers_div">
             <input type="checkbox" name="dhcp" class="onoffswitch-checkbox"
                    id="speakers" <?php if (Speakers::getStatus()) {
                 echo 'checked';
-            } ?>>
+            } ?> />
             <label class="onoffswitch-label" for="speakers">
                 <span class="onoffswitch-inner"></span>
                 <span class="onoffswitch-switch"></span>
             </label>
         </div>
 
-        <button onclick="$.ajax('assets/API/device.php?action=eject');">Eject</button>
-        <button onclick="$.ajax('assets/php/calibrate_screen.php');">Calibrate Screen</button>
-        <button onclick="scanAlbums()">Restore Albums</button>
-        <button class="nuclear" onclick="modal.openPage('assets/modals/format.php');">Factory Reset</button>
-        <button onclick="location.reload();">Refresh</button>
     </div>
 
-    <script>
-        $('#speakers').on("change", function () {
-            if (!this.checked) {
-                $.ajax('assets/API/device.php?action=speakers_off');
-            } else {
-                $.ajax('assets/API/device.php?action=speakers_on');
-            }
-        });
+    <div class="selector calibrate">
+        <i class="fa fa-5x fa-crosshairs"></i>
+        <p>Calibrate Screen</p>
+    </div>
 
-        function scanAlbums() {
-            $.getJSON('/assets/API/import_album.php')
-                .done(function (data) {
-                    if (data.scanned_albums > 0) {
-                        alert(data.scanned_albums + " albums found. " + data.created_albums + " successfully imported.");
-                        reload();
-                    }
-                    else
-                        alert("No albums found.");
-                })
-                .fail(function (error) {
-                    alert("Failed to scan for albums");
-                    console.error(error);
-                });
-        }
-    </script>
+    <br/>
+
+    <div class="selector restore">
+        <i class="fa fa-5x fa-history"></i>
+        <p>Restore Albums</p>
+    </div>
+
+    <div class="selector refresh">
+        <i class="fa fa-5x fa-refresh"></i>
+        <p>Refresh</p>
+    </div>
+
+    <div class="selector reset">
+        <i class="fa fa-5x fa-undo"></i>
+        <p>Factory Reset</p>
+    </div>
 </div>
+
+<script>
+    $('#speakers').on("change", function () {
+        if (!this.checked) {
+            $.ajax('assets/API/system.php?action=speakers_off');
+        } else {
+            $.ajax('assets/API/system.php?action=speakers_on');
+        }
+    });
+
+    $('.selector.eject').click(function () {
+        $.ajax('assets/API/system.php?action=eject');
+    });
+
+    $('.selector.calibrate').click(function () {
+        $.ajax('assets/API/system.php?action=calibrate');
+    });
+
+    $('.selector.restore').click(function () {
+        $.getJSON('/assets/API/import_album.php')
+            .done(function (data) {
+                if (data.scanned_albums > 0) {
+                    alert(data.scanned_albums + " albums found. " + data.created_albums + " successfully imported.");
+                    reload();
+                }
+                else
+                    alert("No albums found.");
+            })
+            .fail(function (error) {
+                alert("Failed to scan for albums");
+                console.error(error);
+            });
+    });
+
+    $('.selector.reset').click(function () {
+        modal.openSettings('format.php');
+    });
+
+    $('.selector.refresh').click(function () {
+        location.reload();
+    });
+</script>
+
+<style>
+    .settings-home .selector {
+        width: 150px;
+        height: 180px;
+        line-height: 30px;
+        vertical-align: bottom;
+    }
+</style>
