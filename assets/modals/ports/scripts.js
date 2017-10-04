d5881c1da6bf7b024afc7c9fe66bb3a0553befff
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-10-04 17:33:37 +0100

    Ports modal is sending the correct format

--- assets/modals/ports/scripts.js
+++ assets/modals/ports/scripts.js
@@ -6,11 +6,17 @@ $(function () {
 
     submit.click(function () {
         const values = form.serializeArray();
+        const valuesLikeTheOthersButBetter = {};
+
+        values.forEach(function (t) {
+            const keyRing = Object.keys(t);
+            valuesLikeTheOthersButBetter[t[keyRing[0]]] = parseInt(t[keyRing[1]]);
+        });
 
         $.ajax({
             url: '/assets/API/ports.php',
             type: 'POST',
-            data: JSON.stringify(values),
+            data: JSON.stringify(valuesLikeTheOthersButBetter),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
             success: function () {
