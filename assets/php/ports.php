7847255bb88b808ba0d99493949eb8b1bc65edae
Stefano Frazzetto <StefanoFrazzetto@users.noreply.github.com>
2017-10-04 19:31:38 +0100

    Apply fixes from StyleCI (#51)

--- assets/php/ports.php
+++ assets/php/ports.php
@@ -3,10 +3,9 @@
  * Created by PhpStorm.
  * User: vittorio
  * Date: 04/10/17
- * Time: 18:59
+ * Time: 18:59.
  */
-
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
 use Lib\Config;
 
